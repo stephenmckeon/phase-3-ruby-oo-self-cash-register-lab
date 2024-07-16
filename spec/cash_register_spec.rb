a15862d922f3c42fd3cf00b501fbc2ffddb76b1e
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-16 14:01:24 -0400

    Solve tests and then some

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -93,10 +93,16 @@ describe 'CashRegister' do
       expect(cash_register.total).to eq(0.99)
     end
 
+    it 'remove the items from items' do
+      cash_register.add_item("apple", 0.99)
+      cash_register.add_item("tomato", 1.76, 2)
+      cash_register.void_last_transaction
+      expect(cash_register.items).to eq(['apple'])
+    end
+
     it 'returns the total to 0.0 if all items have been removed' do
       cash_register.add_item("tomato", 1.76, 2)
       expect { cash_register.void_last_transaction }.to change(cash_register, :total).from(3.52).to(0.0)
     end
   end
 end
-
